373e78a6bc45e49ba65479b827c98b9d3871f4bd
Eric Wilke <eric.wilke@gmail.com>
2021-08-12 13:41:50 -0500

    changed json data structure, got data displaying with handlebars

--- checklistchart.js
+++ checklistchart.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const fs = require('fs')
+const hb = require('express-handlebars')
 
 const app = express()
 const port = process.env.PORT || 3030
 
 app.use(express.json())
+app.engine('handlebars', hb({
+	defaultLayout: 'main'
+}))
+app.set('view engine', 'handlebars')
+
+//const TEMPLATES = JSON.parse(fs.readFileSync("./checklist_chart_data.json"))
+const TEMPLATES = JSON.parse(fs.readFileSync("./template_data.json"))
+console.log(TEMPLATES)
 
 app.get('/', (req,res) => {
 	res.type('text/plain')
-	res.send('Checklist Chart App')
+	res.send('Checklist Chart App: Home Page')
+})
+
+app.get('/chestpain', (req,res) => {
+	const chart = "chest pain"
+	res.render('displayChart', {template: TEMPLATES})
 })
 
 app.listen(port, () => console.log(
