4ff6b935d7b98985c0fc2146d03636fbda9b7cef
Eric Wilke <eric.wilke@gmail.com>
2021-08-15 21:18:44 -0500

    built Handlebars helper to send full data to template for dynamic text build

--- checklistchart.js
+++ checklistchart.js
@@ -5,15 +5,22 @@ const hb = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3030
 
+const TEMPLATES = JSON.parse(fs.readFileSync("./template_data.json"))
+
+//app.use(express.static(__dirname + '/'))
 app.use(express.json())
+
+// The getJSONData helper funtion sends the TEMPLATE data to a handlebars template.
+// The data must be encoded in order to prevent characters destroying the javascript through injection.
 app.engine('handlebars', hb({
-	defaultLayout: 'main'
+	defaultLayout: 'main',
+	helpers: {
+		getJSONData: ()=> {
+			return encodeURIComponent(JSON.stringify(TEMPLATES))
+		}
+	}
 }))
 app.set('view engine', 'handlebars')
-
-//const TEMPLATES = JSON.parse(fs.readFileSync("./checklist_chart_data.json"))
-const TEMPLATES = JSON.parse(fs.readFileSync("./template_data.json"))
-console.log(TEMPLATES)
 
 app.get('/', (req,res) => {
 	res.type('text/plain')
@@ -22,7 +29,10 @@ app.get('/', (req,res) => {
 
 app.get('/chestpain', (req,res) => {
 	// the "chest pain" chart is TEMPLATES.diagnoses[1]
-	res.render('displayChart', {template: TEMPLATES.diagnoses[1]})
+	res.render('displayChart', {
+		template: TEMPLATES.diagnoses[1]
+		}
+	)
 })
 
 app.listen(port, () => console.log(
